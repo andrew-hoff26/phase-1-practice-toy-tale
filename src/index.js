f2a4b6b2a0195426bda6df9d2d61e85011abb420
andrew <andrew_hoffman262@outlook.com>
2023-11-12 02:55:14 -0700

    completed lab

--- src/index.js
+++ src/index.js
@@ -13,3 +13,44 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("http://localhost:3000/toys")
+  .then(response => response.json())
+  .then(toys => {
+    const toyCollection = document.querySelector("#toy-collection");
+
+    toys.forEach(toy => {
+      const card = document.createElement('div');
+      card.className = "card";
+
+      const name = document.createElement('h2');
+      name.textContent = toy.name;
+      if (!toyCollection.contains(card)) {
+        card.appendChild(name);
+        toyCollection.appendChild(card);
+      }
+      const image = document.createElement('img');
+      image.src = toy.image;
+      image.className = ("toy-avatar");
+      const likes = document.createElement("p");
+        likes.textContent = `${toy.likes} Likes`;
+
+        const button = document.createElement("button");
+        button.textContent = "Like";
+        button.addEventListener("click", () => {
+          // Handle like button click event here
+        });
+
+        card.appendChild(name);
+        card.appendChild(image);
+        card.appendChild(likes);
+        card.appendChild(button);
+
+        toyCollection.appendChild(card);
+    })
+  })
+.catch(error => {
+  console.error(error)
+})
+
+})
